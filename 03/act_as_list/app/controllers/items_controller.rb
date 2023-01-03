6d6415939960dcea2749218eff7758a4b09cc0bc
Raquel Ciusz <raquel.ciusz@yahoo.com>
2023-01-03 08:36:04 -0300

    add new item funcionality done

--- 03/act_as_list/app/controllers/items_controller.rb
+++ 03/act_as_list/app/controllers/items_controller.rb
@@ -8,12 +8,30 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: "Item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @item.destroy
     redirect_to items_path, notice: "Item was successfully destroyed."
   end
 
   private
+
+  def item_params
+    params.require(:item).permit(:id, :name)
+  end
+
   def set_item
     @item = Item.find(params[:id])
   end
